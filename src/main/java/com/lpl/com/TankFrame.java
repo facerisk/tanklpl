a03e5164490bd1dbfa32aa4ec47badc5939598f2
facerisk@sina.com <facerisk@sina.com>
2021-01-14 21:22:05 +0800

    窗口每隔50ms刷新为前提，利用键盘控制物体运动

--- src/main/java/com/lpl/com/TankFrame.java
+++ src/main/java/com/lpl/com/TankFrame.java
@@ -44,9 +44,9 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        System.out.println("paint");
+//        System.out.println("paint");
         g.fillRect(x, y, 50, 50);
-        x += 10;
+//        x += 10;
 //        y += 10;
 
     }
@@ -54,17 +54,33 @@ public class TankFrame extends Frame {
     class MyKeyListener extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            x += 200;
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    x -= 10;
+                    break;
+                case KeyEvent.VK_UP:
+                    y -= 10;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    x += 10;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    y += 10;
+                    break;
+                default:
+                    break;
 
-            repaint();
+            }
+
 
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            y += 200;
-
-            repaint();
+//            y += 200;
+//
+//            repaint();
         }
     }
 }

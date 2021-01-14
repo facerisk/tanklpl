aac5408e693e4dc7aff9339dc5728668800d5ee6
facerisk@sina.com <facerisk@sina.com>
2021-01-14 21:04:01 +0800

    封装窗口到对象中使主类中代码更简明，并继承重写键盘监听事件

--- src/main/java/com/lpl/com/Main.java
+++ src/main/java/com/lpl/com/Main.java
@@ -12,18 +12,8 @@ import java.awt.event.WindowEvent;
  */
 public class Main {
     public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800,600);
-        f.setResizable(false);
-        f.setTitle("tank war");
-        f.setVisible(true);
-
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-            }
-        });
+        //封装到TankFram，使代码简洁
+        TankFrame tankFrame = new TankFrame();
 
     }
 }
---
+++ src/main/java/com/lpl/com/TankFrame.java
@@ -0,0 +1,68 @@
+package com.lpl.com;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Classname TankFrame
+ * @Description 继承Frame, 应该算是主类
+ * @Date 2021/1/14 20:00
+ * @Created by lplmbp
+ */
+public class TankFrame extends Frame {
+    int x = 200, y = 200;
+
+    /**
+     * @MethodName TankFrame
+     * @param:
+     * @Return
+     * @Decription 继承后可以直接使用父类方法
+     * @Author lipengliang
+     * @date 2021/1/14 20:04
+     */
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        //键盘监听
+        this.addKeyListener(new MyKeyListener());
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                //实现窗口点击叉关闭
+                System.exit(0);
+            }
+        });
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint");
+        g.fillRect(x, y, 50, 50);
+//        x += 10;
+//        y += 10;
+
+    }
+
+    class MyKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            System.out.println("keypress");
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+            System.out.println("keyreleased");
+        }
+    }
+}

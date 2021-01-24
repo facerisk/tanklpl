45f6b1303da1f1e66127a71d7a861d61f2198aa0
facerisk@sina.com <facerisk@sina.com>
2021-01-24 11:14:31 +0800

    好坦克与坏坦克图片区分

--- src/main/java/com/lpl/tank/ResourceMgr.java
+++ src/main/java/com/lpl/tank/ResourceMgr.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 public class ResourceMgr {
 
     //坦克图片
-    public static BufferedImage tankL,tankU,tankR,tankD;
+    public static BufferedImage goodTankL,goodTankU,goodTankR,goodTankD;
+    //敌方坦克
+    public static BufferedImage badTankL,badTankU,badTankR,badTankD;
     //子弹图片
     public static BufferedImage bulletL,bulletU,bulletR,bulletD;
     //爆炸图片
@@ -21,10 +23,16 @@ public class ResourceMgr {
 
     static{
         try {
-            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
-            tankL = ImageUtil.rotateImage(tankU,-90);
-            tankR = ImageUtil.rotateImage(tankU,90);
-            tankD = ImageUtil.rotateImage(tankU,180);
+            goodTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/GoodTank1.png"));
+            goodTankL = ImageUtil.rotateImage(goodTankU,-90);
+            goodTankR = ImageUtil.rotateImage(goodTankU,90);
+            goodTankD = ImageUtil.rotateImage(goodTankU,180);
+
+
+            badTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
+            badTankL = ImageUtil.rotateImage(badTankU,-90);
+            badTankR = ImageUtil.rotateImage(badTankU,90);
+            badTankD = ImageUtil.rotateImage(badTankU,180);
 
             bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.png"));
             bulletL = ImageUtil.rotateImage(bulletU,-90);

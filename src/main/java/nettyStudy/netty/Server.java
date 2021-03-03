2eebf19a9aad48ea5b9ef8ac90e12e2c5a1ca3b3
facerisk@sina.com <facerisk@sina.com>
2021-03-03 21:10:21 +0800

    netty server优化

--- src/main/java/nettyStudy/netty/Server.java
+++ src/main/java/nettyStudy/netty/Server.java
@@ -1,5 +1,13 @@
 package nettyStudy.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,13 +19,32 @@ import java.net.Socket;
  * @Created by lplmbp
  */
 public class Server {
-    public static void main(String[] args) throws Exception{
-        ServerSocket ss = new ServerSocket();
+    public static void main(String[] args) throws Exception {
+        //负责连接
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //负责工作
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
 
-        ss.bind(new InetSocketAddress("127.0.0.1",8888));
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            System.out.println(socketChannel);
+                        }
+                    })
+                    .bind(8888)
+                    .sync();//bind()为异步方法，所以加同步
+            System.out.println("server started!");
 
-        Socket accept = ss.accept();
+            //阻塞的作用
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
 
-        System.out.println("a client connect!");
     }
 }
